b128f2041cd570ed233ed40f9b2bea9350b34652
Vikram <vikrame@ayraa.io>
2026-01-08 21:31:11 +0530

    Java project

---
+++ GitPracticeEclipseProject/src/com/example/tests/SampleTest1.java
@@ -0,0 +1,12 @@
+package com.example.tests;
+
+public class SampleTest1 {
+
+    public void testOne() {
+        System.out.println("Test One executed");
+    }
+
+    public void testTwo() {
+        System.out.println("Test Two executed");
+    }
+}
---
+++ GitPracticeEclipseProject/src/com/example/tests/SampleTest2.java
@@ -0,0 +1,12 @@
+package com.example.tests;
+
+public class SampleTest2 {
+
+    public void testCreate() {
+        System.out.println("Create test executed");
+    }
+
+    public void testDelete() {
+        System.out.println("Delete test executed");
+    }
+}
