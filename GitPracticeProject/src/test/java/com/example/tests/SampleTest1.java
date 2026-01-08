6a907b3896ab27ff00b38f74cadf883f0fa7d915
Vikram <vikrame@ayraa.io>
2026-01-08 21:19:24 +0530

    2nd commit

---
+++ GitPracticeProject/src/test/java/com/example/tests/SampleTest1.java
@@ -0,0 +1,16 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class SampleTest1 {
+
+    @Test
+    public void testLogin() {
+        System.out.println("Executing testLogin");
+    }
+
+    @Test
+    public void testLogout() {
+        System.out.println("Executing testLogout");
+    }
+}
---
+++ GitPracticeProject/src/test/java/com/example/tests/SampleTest2.java
@@ -0,0 +1,16 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class SampleTest2 {
+
+    @Test
+    public void testCreateUser() {
+        System.out.println("Executing testCreateUser");
+    }
+
+    @Test
+    public void testDeleteUser() {
+        System.out.println("Executing testDeleteUser");
+    }
+}
